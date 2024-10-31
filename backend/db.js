c63c7ac871d3bc176e84cdcac26879dee99492ad
henningrm <henning.mielkau@gmail.com>
2024-10-31 10:36:00 +0800

    Updated db, added docker file for GCP deployment

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: 5432,
 });
 
 module.exports = pool;
